1c75d83c15ad4e52929f27f50980b209f442dbb6
Tim <konick781@gmail.com>
2022-09-11 22:15:45 +0000

    Version

--- cookbooks/mydocker/metadata.rb
+++ cookbooks/mydocker/metadata.rb
@@ -7,7 +7,7 @@ long_description 'Installs/Configures mydocker'
 version '0.1.0'
 chef_version '>= 14.0'
 
-depends 'docker', '~>10.1.6'
+depends 'docker', '~>4.0.0'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
